7d0f8875ae66221efec68dd9357ec2e5bedbbc4b
liao2000 <liao2000@livemail.tw>
2021-03-19 22:13:12 +0800

    First commit

---
+++ HW2/4107056019-assign-02-element.py
@@ -0,0 +1,40 @@
+def factorial(i):
+    ret = 1
+    for i in range(1, i+1):
+        ret *= i
+    return ret
+
+
+def c(p, q):
+    if p < q:
+        return 0
+    j = 1
+    for i in range(q+1, p+1, 1):
+        j = j * i
+    return j // factorial(p-q)
+
+print('please input n k m')
+n2, k2, m2 = input().split()
+n = int(n2)
+k = int(k2)
+m = int(m2)
+if n < 1 or n > 81 or k < 1 or k > n or m < 0 or m > c(n,k)-1:
+    print("Error (1<=n<=81 1<=k<=n 0<=m<=C(n,k)-1)")
+    exit()
+
+m = c(n , k) - 1  - m
+r = m
+s = []
+j = n
+for step in range(k):
+    i = j-1
+    while i >= k-1-step:
+        # decreasing order
+        if c(i, k-step) <= r:
+            break
+        i = i-1
+    j = i
+    s.append(n - 1 - i)
+    r = r-c(j, k-step)
+
+print(s)
---
+++ README.md
@@ -0,0 +1,23 @@
+# Color science NCHU
+
+## HW1
+
+> Input: n, k, m
+>
+> n decided the domain [0, n)
+>
+> k decided the length of tuple
+>
+> m is the m-th cobidic tuple
+>
+> n = 5, k = 3
+>
+> ![](./assets/cobidic.png)
+> 
+
+## HW2
+
+HW2 is simary to HW1, but return the m-th element tuple
+
+## HW3
+// TODO
